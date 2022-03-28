a0bdba67f9338a0d6dfd4e2e1572b45ad2e1e2b5
unknown <bensonmakau2000@gmail.com>
2022-03-28 14:11:30 +0300

    created a variable in js and printing it in console

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let js = 'amazing';
-if(js === 'amazing') alert('This is amazing');
-// console.log(10+15+8-6)
+//Printing values to console
+console.log(40+8+23-10)
+
+//Declaring a Variable
+let firstName = "Benson"
+console.log(firstName);//printing a variable in the console
