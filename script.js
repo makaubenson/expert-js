61e2ef8162919eb690ff372303e8263e928ce5b4
unknown <bensonmakau2000@gmail.com>
2022-03-28 14:38:07 +0300

    completed coding challenge 1

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 //Printing values to console
-console.log(40+8+23-10)
+// console.log(40+8+23-10)
 
 //Declaring a Variable
-let firstName = "Benson"
-console.log(firstName);//printing a variable in the console
+// let firstName = "Benson"
+// console.log(firstName);//printing a variable in the console
 
+//Coding challenge 1
+markHeight = 1.69;
+markWeight = 78;
+
+johnHeight= 1.95;
+johnWeight = 92;
+
+//BMI = mass / height ** 2
+markBMI = markWeight / markHeight ** 2
+markBMI2 = markWeight / (markHeight * markHeight)
+console.log(markBMI)
+console.log(markBMI2)
+
+//BMI = mass / height ** 2
+johnBMI = johnWeight / johnHeight ** 2
+johnBMI2 = johnWeight / (johnHeight * johnHeight)
+console.log(johnBMI)
+console.log(johnBMI2)
+ let markHigherBMI = '';
+ if(markBMI >johnBMI ) {
+     markHigherBMI = true
+ }else{
+     markHigherBMI = false
+ }
+
+
+ //Javascr
