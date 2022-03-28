3a8a6acdb90dbb84bcaccec13ab0f401d551c2d5
unknown <bensonmakau2000@gmail.com>
2022-03-28 16:09:59 +0300

    multiplication,division,power

--- script.js
+++ script.js
@@ -58,3 +58,7 @@ const now = 2037;
 const ageJonas = now-1991;
 const ageSarah = now-2018;
 console.log(ageJonas, ageSarah);
+
+//Multiply
+console.log(ageJonas*2, ageJonas/10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2*2*2
