711f680a04a9d4d58630f2e188b4d4412bdf550c
unknown <bensonmakau2000@gmail.com>
2022-03-28 16:11:46 +0300

    addition sign
    used for concatination

--- script.js
+++ script.js
@@ -62,3 +62,7 @@ console.log(ageJonas, ageSarah);
 //Multiply
 console.log(ageJonas*2, ageJonas/10, 2 ** 3);
 //2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Benson";
+const lastName = "Makau";
+console.log(firstName + ' '+ lastName);
