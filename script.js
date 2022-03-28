c74f942f8cd50b129f996803ca25bb953246da57
unknown <bensonmakau2000@gmail.com>
2022-03-28 16:16:13 +0300

    assignment operators
     = and += signs

--- script.js
+++ script.js
@@ -57,12 +57,17 @@ job= 'CEO';
 const now = 2037;
 const ageJonas = now-1991;
 const ageSarah = now-2018;
-console.log(ageJonas, ageSarah);
+// console.log(ageJonas, ageSarah);
 
 //Multiply
-console.log(ageJonas*2, ageJonas/10, 2 ** 3);
+// console.log(ageJonas*2, ageJonas/10, 2 ** 3);
 //2 ** 3 means 2 to the power of 3 = 2*2*2
 
-const firstName = "Benson";
-const lastName = "Makau";
-console.log(firstName + ' '+ lastName);
+// const firstName = "Benson";
+// const lastName = "Makau";
+// console.log(firstName + ' '+ lastName);
+
+//ASSIGNMENT OPERATORS
+let x = 10+5;//15
+x+=10;// x= x+10
+console.log(x);// 25
