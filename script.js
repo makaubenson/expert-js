84424813b7eaea1b00707b732b1ebe4377aba9b4
unknown <bensonmakau2000@gmail.com>
2022-04-01 17:28:52 +0300

    strict operator in use

--- script.js
+++ script.js
@@ -231,8 +231,13 @@ console.log(typeof favorite);
 
 if(favorite == 23){
     console.log('Cool! 23 is an amazing number!');
+}else if(favorite === 7){
+    console.log('7 is also a cool number!');
+}else if(favorite === 9){
+    console.log('9 is also a cool number!');
+}else{
+    console.log('Number is neither 23 nor 7');
 }
 
-if(favorite === 23){
-    console.log('Cool! 23 is an amazing number!');
-}
+//Is Not Equal To Strict Operator (!==)
+if(favorite !== 23) console.log('Why not 23?');
