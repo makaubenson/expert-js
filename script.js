a3ca0348034447c4ee35bc45a753f3f2d7ca1030
unknown <bensonmakau2000@gmail.com>
2022-04-02 05:38:55 +0300

    If Else version of the Switch Case

--- script.js
+++ script.js
@@ -370,3 +370,18 @@ switch (day) {
         default:
             console.log('Not a valid day');
 }
+//If Else version of the Switch Case
+if(day === 'monday'){
+    console.log('Plan Course Structure');
+    console.log('Go to coding meetup');
+}else if(day === 'tuesday'){
+    console.log('Prepare theory videos');
+}else if(day === 'wednesday' || day === 'thursday' ){
+    console.log('Write code examples');
+}else if(day === 'friday'){
+    console.log('Record videos');
+}else if(day === 'saturday' || day === 'sunday' ){
+    console.log('Enjoy the weekend');
+}else{
+    console.log('Not a valid day');
+}
